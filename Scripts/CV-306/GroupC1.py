0efe286ba81732325066c0a34c15240abc6298f0
Chanse Strode <chansestrode@gmail.com>
2020-05-02 20:17:18 -0500

    updated the C test to a try catch

--- Scripts/CV-306/GroupC1.py
+++ Scripts/CV-306/GroupC1.py
@@ -25,25 +25,53 @@ for teamId in listC1:
     navToSchool.click()
 
     #Check if business unit is elite
-    isItAnEliteTeam = driver.find_element_by_class_name("business-unit")
-    whatIsIt = isItAnEliteTeam.text
-    if(whatIsIt == "Elite"):
-        errorState(ListNumber,teamId,"Elite School",totalTeams)
+    try:
+        isItAnEliteTeam = driver.find_element_by_class_name("business-unit")
+        whatIsIt = isItAnEliteTeam.text
+        print("Business Unit: " + whatIsIt)
+        if(whatIsIt == "Elite"):
+            errorState(ListNumber,teamId,"Elite School",totalTeams)
+            driver.close()
+            continue
 
-    elif(whatIsIt != "Elite"):
+        else:
+            print("Not an Elite School " + "Business Unit: " + whatIsIt)
+
+    except:
+        errorState(ListNumber,teamId,"Couldn't Find Business Unit",totalTeams)
+        driver.close()
+        continue
+
+    try:
         #Check if they have a sportscode licens
         navToSportsCode = driver.find_element_by_xpath("//a[contains(@href,'/admin/sportscode/licenses/')]")
         navToSportsCode.click()
         time.sleep(1)
-        try:
-            doTheyHaveASportsCodeLicenses = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[2][contains(string(), '(this School has no Licenses)')]")
-            wellDoThey = doTheyHaveASportsCodeLicenses.text
+        print("Was able to navigate to sports code page...")
 
-            if(wellDoThey == "(this School has no Licenses)"):
-                errorState(ListNumber,teamId,"does NOT have SportsCode Licenses",totalTeams)
+    except:
+        errorState(ListNumber,teamId,"Couldn't navigate to sports code page",totalTeams)
+        driver.close()
+        continue
+
+    try:
+        doTheyHaveASportsCodeLicenses = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[2][contains(string(), '(this School has no Licenses)')]")
+        wellDoThey = doTheyHaveASportsCodeLicenses.text
+
+        if(wellDoThey == "(this School has no Licenses)"):
+            errorState(ListNumber,teamId,"does NOT have SportsCode Licenses",totalTeams)
+            driver.close()
             continue
-            
-        except:
+        else:
+            print("Has sports code licenses")
             passingState(ListNumber,teamId,totalTeams)
+            driver.close()
+            continue
+
+    except:
+        print("Made it to the last except")
+        passingState(ListNumber,teamId,totalTeams)
+        driver.close()
+        continue
 
     driver.close()
