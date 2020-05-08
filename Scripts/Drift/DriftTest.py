7877f7a1e3a9392b03631177db832d152c4f978a
Chanse Strode <chansestrode@gmail.com>
2020-05-07 19:09:47 -0500

    first draft finished

--- Scripts/Drift/DriftTest.py
+++ Scripts/Drift/DriftTest.py
@@ -8,26 +8,44 @@ import datetime
 driver = webdriver.Chrome()
 driver.get(thorenv)
 time.sleep(1)
+
 #Load stream info
 inputElement = driver.find_element_by_xpath("//*[@id='player-stream-src']")
 inputElement.clear()
 inputElement.send_keys(videoSrc)
 submitButton = driver.find_element_by_id("player-stream-load").click()
 time.sleep(3)
-#TODO: Select 2x speed
+
+#Select 2x speed
 setTheSpeed = driver.find_element_by_xpath("//*[@id='player-playbackrate']/option[5]").click()
 
-#TODO: Get current Time
-currentTime = driver.find_element_by_id("player-current-time")
-currentTime.get_parameter('Value')
-print(str(currentTime))
-#TODO: GET EXPECTED DURATION
+#Get current Time
+currentTime = driver.execute_script("return document.getElementById('player-current-time').value;")
+currentTimeString = str(int(float(currentTime)))
 
-#TODO: GET drift and convert from ms to seconds
+#Get expected duration
+expectedTime = driver.execute_script("return document.getElementById('player-expected-duration').value;")
+expectedTimeString = str(int(float(expectedTime)))
 
-#TODO: write a while loop that checks if current time = duration and continually update the drift ammount.
 
-#TODO: print/write out drift difference to a file. 
+#Write a while loop that checks if current time = duration and continually update the drift ammount.
+while (currentTimeString != expectedTimeString):
+    currentTime = driver.execute_script("return document.getElementById('player-current-time').value;")
+    currentTimeString = str(int(float(currentTime)))
+
+    #Get drift and convert from ms to seconds
+    driftTime = driver.execute_script("return document.getElementById('drift').value;")
+    msToSeconds = float(driftTime) / 1000
+
+    print ("Current Time: " + currentTimeString + " in Seconds")
+    print ("Current Drift: " + str(float(msToSeconds)) + " in Seconds")
+    time.sleep(60)
+
+print("Finished!")
+print ("Final Time: " + currentTimeString + " in Seconds")
+print ("Final Drift: " + str(float(msToSeconds)) + " in Seconds")
+
+#TODO: print/write out drift difference to a file.
 
 #TODO: decide if we want to wrap it all in a for loop to test multiple videos
 driver.close()
