339f7d3b85615b5f73b7e083511b9cb644b21d53
Chanse Strode <chansestrode@gmail.com>
2020-05-11 15:11:04 -0500

    fixed it

--- Scripts/Drift/DriftTest.py
+++ Scripts/Drift/DriftTest.py
@@ -20,10 +20,10 @@ time.sleep(3)
 setTheSpeed = driver.find_element_by_xpath("//*[@id='player-playbackrate']/option[5]").click()
 
 
-currentTimeString = "intial set 1"
+currentTimeString = ""
 print(currentTimeString + "first time being set not in loop 1")
 
-currentTimeSecondCheckString = "intial set 2"
+currentTimeSecondCheckString = "2"
 print(currentTimeSecondCheckString +  "first time being set not in loop 1")
 
 driftTime = driver.execute_script("return document.getElementById('drift').value;")
@@ -31,39 +31,34 @@ msToSeconds = float(driftTime) / 1000
 #Get current Time
 def firstTimeCheck():
     time.sleep(1)
+    global currentTimeString
     currentTime = driver.execute_script("return document.getElementById('player-current-time').value;")
     currentTimeString = str(int(float(currentTime)))
     print(currentTimeString)
 
-
-
 def secondTimeCheck():
     time.sleep(3)
+    global currentTimeSecondCheckString
     currentTimeSecondCheck = driver.execute_script("return document.getElementById('player-current-time').value;")
     currentTimeSecondCheckString = str(int(float(currentTimeSecondCheck)))
     print(currentTimeSecondCheckString)
 
 #Write a while loop that checks if current time = duration and continually update the drift ammount.
 #TODO the current time and play time will be different because of drift... we need a way to confirm that they should match.
-while currentTime != currentTimeSecondCheck:
+while currentTimeString != currentTimeSecondCheckString:
     firstTimeCheck()
-    currentTimeString
-    print(currentTimeString + "loop run 1")
-    secondTimeCheck()
-    print(currentTimeSecondCheckString + "loop run 2")
-    currentTimeSecondCheckString
+    print(currentTimeString)
 
+    secondTimeCheck()
+    print(currentTimeSecondCheckString)
 
     #Get drift and convert from ms to seconds
     driftTime = driver.execute_script("return document.getElementById('drift').value;")
     msToSeconds = float(driftTime) / 1000
 
-
     print ("Current Time: " + str(currentTimeString) + " in Seconds")
     print ("Current Drift: " + str(float(msToSeconds)) + " in Seconds")
     time.sleep(5)
-
-
 
 print("Finished!")
 print ("Final Time: " + currentTimeString + " in Seconds")
